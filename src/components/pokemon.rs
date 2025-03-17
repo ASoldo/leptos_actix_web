f7839c7db577e1cd156fc0d9b158b841543c1926
ASoldo <soldo.andrej@gmail.com>
2025-03-18 00:59:34 +0100

    Update: Fetch example with Action

--- src/components/pokemon.rs
+++ src/components/pokemon.rs
@@ -10,26 +10,27 @@ pub struct Pokemon {
 
 #[server(GetPokemon, "/api")]
 pub async fn get_pokemon() -> Result<Pokemon, ServerFnError> {
-    let poke = reqwest::get("https://pokeapi.co/api/v2/pokemon/25")
+    Ok(reqwest::get("https://pokeapi.co/api/v2/pokemon/25")
         .await?
         .json::<Pokemon>()
-        .await?;
-    Ok(poke)
+        .await?)
 }
 
 #[component]
 pub fn PokemonView() -> impl IntoView {
-    let pokemon_resource = Resource::new(|| (), |_| async move { get_pokemon().await });
+    let fetch_pokemon = Action::new(|_| async move { get_pokemon().await });
 
     view! {
-        <Suspense>
-            <div>
-                {move || match pokemon_resource.get() {
-                    None => view! { <p>{"Loading...".to_string()}</p> },
-                    Some(Ok(poke)) => view! { <p>{format!("Pokémon Name: {}", poke.name)}</p> },
-                    Some(Err(err)) => view! { <p>{format!("Error: {}", err)}</p> },
-                }}
-            </div>
-        </Suspense>
+        <div>
+            <button class="bg-blue-500 rounded text-white p-4 m-2" on:click=move |_| { fetch_pokemon.dispatch(()); }>
+                "Fetch Pokémon"
+            </button>
+
+            {move || match fetch_pokemon.value().get() {
+                None => view! { <p>{"".to_string()}</p> },
+                Some(Ok(poke)) => view! { <p>{format!("Pokémon Name: {}", poke.name)}</p> },
+                Some(Err(err)) => view! { <p>{format!("Error: {}", err)}</p> },
+            }}
+        </div>
     }
 }
